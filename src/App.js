a086508ccd50bbe9c1a707f3253418fee06d71ac
Deaconjoe <clouddjoe1@gmail.com>
2019-12-03 17:43:00 +0000

    feature added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello hi there every one in!! my crew 
+          hello hi there every one in!! my crew I was changed on the feature branch
         </p>
         <a
           className="App-link"
