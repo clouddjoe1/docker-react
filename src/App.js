d55b12980ee834e6b0621439f5fcf7c4cd44b8b4
Deaconjoe <clouddjoe1@gmail.com>
2019-12-03 18:32:42 +0000

    1

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        TITLE: THE JOY OF WORSHIP
+KEY SCRIPTURE: PSALM 84
+
+Many Christians find no joy in worship. Many feels that they must "have
+their toes stepped on" in order to get anything out of worship. Others feel
+that they have sinned if they feel happy about worship. They say, "I didn't
+come to get entertained." This has led to perversions in worship. Some
+have sought a wild emotional experience. Others have gone the opposite
+route, either going into legalism and cold formality or not worshiping at
+all.
+
+In Psalm 84 the psalmist shows us the proper attitude of God's children
+toward worship. Worship is a great joy for the true child of God. It is an
+opportunity to come before God and express our gratitude for the
+greatness of God and the salvation that He has given as a gift to us. In
+Psalm 84, the Psalmist shared his trust in the Lord. Our heartfelt trust in
+God is the foundation of the joy in worship. When we trust in the Lord,
+our worship is more meaningful.
+
+Discussion Point
+
+1. What were the key things to note in David’s worship time in
+Psalm 84?
+a) Psalm 84:1, Matthew 18:20
+b) Psalm 84:2, Psalm 122:1
+c) Psalm 42:1-2, Matt 5:6
+d) Psalm 84:4, Heb. 10:25
+2. What are the blessings highlighted in Psalm 84: 4-7?
+a) Psalm 84:4, Rom 12:12
+b) Psalm 84:5, 7
+c) Psalm 84:6, 2 Tim 2:19
+3. What can we learn from David’s prayer of praise in Psalm 84?
+a) Psalm 84:8-9
+b) Psalm 84: 10
+c) Psalm 84:11, 115:9
+d) Psalm 84:12
+4) How will todays lesson change your attitude to worship in Gods
+house?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
